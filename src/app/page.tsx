15a791f69069957edbcebc5bcb01c26cde4a3efe
Jim Merritt <merritt.ja@gmail.com>
2025-01-07 13:59:24 -0500

    Adding dark mode and a11y considerations for text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 		>
 			<header className="md:col-span-2 row-start-1">
 				<h1 className="m-0 ml-[-1.65rem] p-0 leading-none">Jim Merritt</h1>
-				<p className="m-0 p-0 leading-none uppercase text-brand">
+				<p className="m-0 p-0 leading-none uppercase text-[--brand-text]">
 					Senior Frontend Developer
 				</p>
 			</header>
