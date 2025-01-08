4165466afe7101b244aa3add6e55874c41144dfc
Jim Merritt <merritt.ja@gmail.com>
2025-01-08 08:23:06 -0500

    Updating layout to highlight experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 	return (
 		<main
 			id="main-content"
-			className="max-w-5xl py-12 px-16 grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-6 auto-cols-min"
+			className="max-w-5xl py-12 px-16 grid gap-x-12 gap-y-6 grid-cols-1 md:grid-cols-2 md:grid-rows-[auto_100px_auto]"
 		>
 			<header className="md:col-span-2 row-start-1">
 				<h1 className="m-0 ml-[-1.65rem] p-0 leading-none">Jim Merritt</h1>
@@ -18,72 +18,8 @@ export default function Home() {
 					Senior Frontend Developer
 				</p>
 			</header>
-			<section className="max-w-lg col-start-1" aria-labelledby="contact">
-				<h2 id="contact">Contact</h2>
-				<nav>
-					<ul className="m-0 p-0 list-none">
-						<li>
-							<a
-								href="https://github.com/JimMerritt"
-								aria-label="Jim Merritt on GitHub"
-							>
-								GitHub
-							</a>
-						</li>
-						<li>
-							<a
-								href="https://www.linkedin.com/in/jim-merritt-b3240a65/"
-								aria-label="Jim Merritt on LinkedIn"
-							>
-								LinkedIn
-							</a>
-						</li>
-					</ul>
-				</nav>
-			</section>
-			<section className="max-w-lg md:col-start-2" aria-labelledby="education">
-				<h2 id="education">Education</h2>
-				<h3>Northern Kentucky University</h3>
-				<p>Visual Communications Design</p>
-				<p>Minor in Computer Science</p>
-			</section>
-			<section className="max-w-lg col-start-1" aria-labelledby="skills">
-				<h2 id="skills">Skills</h2>
-				<div className="grid grid-cols-1 xl:grid-cols-2 gap-x-12 gap-y-6">
-					<dl>
-						<dt>Core Technical</dt>
-						<dd>
-							<ul className="m-0 p-0 list-none">
-								<li>TypeScript</li>
-								<li>JavaScript</li>
-								<li>React</li>
-								<li>React Native</li>
-								<li>Node</li>
-								<li>PWA Architecture</li>
-								<li>CI/CD</li>
-								<li>UI/UX</li>
-								<li>Next</li>
-								<li>HTML5</li>
-								<li>CSS3</li>
-							</ul>
-						</dd>
-					</dl>
-					<dl>
-						<dt>Team & Architectural</dt>
-						<dd>
-							<ul className="m-0 p-0 list-none">
-								<li>Frontend Architecture</li>
-								<li>Agile Development</li>
-								<li>Team Development</li>
-								<li>Cross-Functional Collaboration</li>
-								<li>Code Mentoring</li>
-							</ul>
-						</dd>
-					</dl>
-				</div>
-			</section>
 			<section
-				className="max-w-lg md:col-start-2 md:row-start-3"
+				className="max-w-lg md:row-start-2 md:row-span-2"
 				aria-labelledby="experience"
 			>
 				<h2 id="experience">Experience</h2>
@@ -146,6 +82,70 @@ export default function Home() {
 						graphic design, logo design and brand development.
 					</p>
 				</article>
+			</section>
+			<section
+				className="max-w-lg md:col-start-2 md:row-start-3"
+				aria-labelledby="skills"
+			>
+				<h2 id="skills">Skills</h2>
+				<div className="grid grid-cols-1 xl:grid-cols-2 gap-x-12 gap-y-6">
+					<dl>
+						<dt>Core Technical</dt>
+						<dd>
+							<ul className="m-0 p-0 list-none">
+								<li>TypeScript</li>
+								<li>JavaScript</li>
+								<li>React</li>
+								<li>React Native</li>
+								<li>Node</li>
+								<li>PWA Architecture</li>
+								<li>CI/CD</li>
+								<li>UI/UX</li>
+								<li>Next</li>
+								<li>HTML5</li>
+								<li>CSS3</li>
+							</ul>
+						</dd>
+					</dl>
+					<dl>
+						<dt>Team & Architectural</dt>
+						<dd>
+							<ul className="m-0 p-0 list-none">
+								<li>Frontend Architecture</li>
+								<li>Agile Development</li>
+								<li>Team Development</li>
+								<li>Cross-Functional Collaboration</li>
+								<li>Code Mentoring</li>
+							</ul>
+						</dd>
+					</dl>
+				</div>
+			</section>
+			<section
+				className="max-w-lg md:col-start-2 md:row-start-2"
+				aria-labelledby="contact"
+			>
+				<h2 id="contact">Contact</h2>
+				<nav>
+					<ul className="m-0 p-0 list-none">
+						<li>
+							<a
+								href="https://github.com/JimMerritt"
+								aria-label="Jim Merritt on GitHub"
+							>
+								GitHub
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.linkedin.com/in/jim-merritt-b3240a65/"
+								aria-label="Jim Merritt on LinkedIn"
+							>
+								LinkedIn
+							</a>
+						</li>
+					</ul>
+				</nav>
 			</section>
 		</main>
 	);
